a69535cb4fe9204c1abc1b01f16df0a28e2b245c
HomeHP <maksshos05@gmail.com>
2023-08-06 20:02:42 +0300

    FIRST VERSION OF SITE. CONGRATULATIONS MY FRIENDS U+1F37E

--- scripts/contact_us_popup.js
+++ scripts/contact_us_popup.js
@@ -2,17 +2,31 @@ const openBtns = document.querySelectorAll(".open-contact");
 const overlay = document.getElementById("popup-overlay");
 const popup = document.getElementById("popup");
 
+function disableScroll(){
+    const x = window.scrollX;
+    const y = window.scrollY;
+    window.onscroll = function (){
+        window.scrollTo(x,y);
+    };
+}
+function enableScroll(){
+    window.onscroll = null;
+}
+
+
 openBtns.forEach((openBtn) =>{
     openBtn.addEventListener("click", () => {
         overlay.classList.add("active");
         popup.classList.toggle("opened");
         closeMenu();
         closeMenuMobile();
+        disableScroll();
     });
 });
 
 overlay.addEventListener("click", (event) => {
     if (event.target === overlay) {
+        enableScroll();
         popup.classList.remove("opened");
         setTimeout(function(){
             overlay.classList.remove("active");
